dd61dc44fb8d410ad6c70277321febe61e581574
Sur091 <91460166+Sur091@users.noreply.github.com>
2023-05-04 07:22:21 +0545

    The first commit

---
+++ learning_rust/target/debug/deps/learning_rust-59da6f15f130f902.d
@@ -0,0 +1,5 @@
+D:\Rust\learning_rust\target\debug\deps\learning_rust-59da6f15f130f902.rmeta: src\main.rs
+
+D:\Rust\learning_rust\target\debug\deps\learning_rust-59da6f15f130f902.d: src\main.rs
+
+src\main.rs:
---
+++ learning_rust/target/debug/learning_rust.d
@@ -0,0 +1,1 @@
+D:\Rust\learning_rust\target\debug\learning_rust.exe: D:\Rust\learning_rust\src\main.rs
